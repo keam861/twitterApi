be70b82c4fb4d7eef4a5ac1e71a1224108db4bff
codeamac08 <codeamac08@gmail.com>
2016-01-22 14:14:43 -0600

    mejorando la Ux

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -7,7 +7,6 @@ end
 get "/:handle" do #placeholder in this case params[:twitterhandle]
   
   handle = params[:handle] #input.value
-  
   @handle = TwitterUser.find_by(user: handle)
   if @handle
     puts '=' * 50
@@ -26,33 +25,13 @@ get "/:handle" do #placeholder in this case params[:twitterhandle]
     
   end
   
-  
-   erb :index
+  erb :_twee, layout: false
 end
 
-
-post '/fetch' do
-  
-   # handle = params[:twitterhandle]
-  
-  # @handle = TwitterUser.find_by(user: handle)
-  # if @handle
-  #   puts '=' * 50
-  #   puts 'handle = '+handle+' exist !!!!'
-  #   puts  @handle.id
-  #   @tweets = @handle.tweets
-  # else
-  #   puts '=' * 50
-  #   puts 'handle = '+handle+' create !!!'
-  #   tu = TwitterUser.create(user: handle)
-  #   @tweets = []
-  #   arr = CLIENT.user_timeline(handle)
-  #   arr.each do |tweet|
-  #      @tweets << Tweet.create(twitter_user_id: tu.id ,tuit: tweet.text)
-  #   end
-    
-  # end
-  redirect to ("/#{params[:twitterhandle]}")
-  
+post "/:value" do 
+  tuit = params[:value]
+  CLIENT.update(tuit)
+#   puts value
 
 end
+
--- public/js/application.js
+++ public/js/application.js
@@ -1,34 +1,64 @@
 $(document).ready(function() {
-	// Este código corre después de que `document` fue cargado(loaded) 
-	// completamente. 
-	// Esto garantiza que si amarramos(bind) una función a un elemento 
-	// de HTML este exista ya en la página. 
-
+  spiner();
+  loadTweets();
+  tweet();
 });
 
-
-$(document).ready(function(){
-    $("#spinner").bind("ajaxSend", function() {
-        $(this).show();
-    }).bind("ajaxStop", function() {
-        $(this).hide();
-    }).bind("ajaxError", function() {
-        $(this).hide();
-  });
- 
- });
-
-$(document).ready(function(){
+function spiner() {
     $('#submit').click(function() {
         $('#spinner').show();
     });
-});
+}
 
-// function loadtweets(){
-   
-//     $("#tweets").append("
-//       <% @tweets.each do |tweet| %>
-//         <p><%= tweet.tuit %></p>
-//       <% end %>");
-    
-// }
+function hideSpin() {
+    $('#spinner').hide();
+}
+
+function loadTweets() {
+   $('#begin').submit(function() {
+    event.preventDefault();
+      value = getInput("Tname").value;   
+      url = "/" + value
+      $.get( url, function( data ) {
+      $("div#tweets").html(data);
+      hideSpin();
+    });
+   });
+}
+
+function getInput(input) {
+   return document.getElementById(input);
+}
+
+function postOutPut() {
+  $("#message").html("Tweet Successful");
+}
+
+function tweet() {
+// =========================pseudocode=========================
+  // post '/tweet' do
+  //   # Recibe el input del usuario
+
+  //   # Crea el tweet utilizando la API de Twitter
+
+  //   # Regresa al usuario el tweet o un mensaje de EXITO o ERROR
+  // end
+  $('#tweet').submit(function() {
+    event.preventDefault();
+      value = getInput("twee").value; 
+      if (value != "") {
+        url = "/" + value
+        $.post( url, function( data ) {
+        // console.log("this should tweet :");
+        // console.log(typeof value);
+        // console.log("data :");
+        // console.log(data =="");
+        $("#message").html("Tweet Successfully created");
+         
+        });
+       
+      } else {
+        $("#message").html("You should write a tweet");
+      };  
+  });
+}
